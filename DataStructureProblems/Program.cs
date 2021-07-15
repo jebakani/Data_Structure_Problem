d5fe5109838002c0fee3d831ebc2fb66582d47f0
Jebakani <vjishwarya@gmail.com>
2021-07-15 12:05:41 +0530

    [JEBAKANI] Executing Data structure problem

---
+++ DataStructureProblems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructureProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating data structure problem");
+        }
+    }
+}
