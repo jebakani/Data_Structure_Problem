4dc31df3a07368a1dbc0d3ab9f293d4f6298bc5b
Jebakani <vjishwarya@gmail.com>
2021-07-15 12:29:12 +0530

    [JEBAKANI] Searching the word in text file using unordered list

---
+++ UnorderedLIst/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using UnOrderedList;
+
+namespace UnorderedLIst
+{
+    class Program
+    {
+        // <summary>
+        /// P1-Linked list to read input from file and seaching for a word in the list 
+        /// if present remove the word else insert the word 
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            UnOrderedLinkedList<string> unOrdered = new UnOrderedLinkedList<string>();
+            string filePath = @"C:\Users\HP1\source\repos\DS\UnorderedLIst\UnorderedListFile.txt";
+            string text = File.ReadAllText(filePath);
+            string[] textArray = text.Split(" ");
+            for (int i = 0; i < textArray.Length; i++)
+            {
+                unOrdered.AddLast(textArray[i]);
+            }
+            Console.WriteLine("enter the word to be searched:");
+            string word = Console.ReadLine();
+            int found = unOrdered.SearchNode(word);
+            if (found == 1)
+            {
+                unOrdered.DeleteNode(word);
+            }
+            else
+            {
+                unOrdered.AddLast(word);
+            }
+            string newText = unOrdered.DisplayList();
+            File.WriteAllText(filePath, newText);
+        }
+    }
+}
