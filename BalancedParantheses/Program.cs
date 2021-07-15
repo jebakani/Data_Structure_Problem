3ffff330bf865b58317f9ec0dd951d24784711e0
Jebakani <vjishwarya@gmail.com>
2021-07-15 13:11:41 +0530

    [JEBAKANI] Balancing paranthesis using stack

---
+++ BalancedParantheses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BalancedParantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int unbalanced=0;
+            StackImplementation<char> stack = new StackImplementation<char>();
+            string equation = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)";
+            for(int i=0;i<equation.Length;i++)
+            {
+                if(equation[i].Equals('('))
+                {
+                    stack.Push(equation[i]);
+                }
+                else if(equation[i].Equals(')'))
+                {
+                    unbalanced = stack.pop();
+                    if(unbalanced==1)
+                    {
+                        break;
+                    }
+                }
+            }
+            if(stack.CheckTop()==1)
+            {
+                Console.WriteLine("Equation is balanced");
+            }
+            else if(stack.CheckTop()==0 && unbalanced==0)
+            {
+                Console.WriteLine("Equation is unbalanced");
+            }
+         }
+    }
+}
---
+++ BalancedParantheses/StackImplementation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedParantheses
+{
+    
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> Next;
+        public Node(T value)
+        {
+            this.value = value;
+            this.Next = null;
+        }
+    }
+        
+    class StackImplementation<T> 
+    {
+        public Node<T> top;
+
+        public void Push(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            if(top==null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+        }
+
+        public int pop()
+        {
+            if(top==null)
+            {
+                Console.WriteLine("Unbalanced");
+                return 0;
+
+            }
+            else
+            {
+                top = top.Next;
+                return 1;
+            }
+        }
+
+        public int CheckTop()
+        {
+            if(top==null)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
