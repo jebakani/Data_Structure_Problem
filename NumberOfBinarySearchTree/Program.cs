e5572f6445e85b5ee765dafce72d78f156ba5db8
Jebakani <vjishwarya@gmail.com>
2021-07-16 17:38:44 +0530

    [JEBAKANI] Finding number of tree that can be formed from n number of node

---
+++ NumberOfBinarySearchTree/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NumberOfBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of Binary Search tree with n node");
+            int trees=NumberOfBST.NumberOfTrees(3);
+            Console.WriteLine("Total number of tress ={0}",trees);
+        }
+    }
+}
